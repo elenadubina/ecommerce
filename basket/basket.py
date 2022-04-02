2cb45a7a90864ad2b8b4527275927eb8649dcb39
Elena Dubina <elena.dubina89@gmail.com>
2022-04-02 17:17:27 +0300

    Deleting basket/session data

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,7 @@
+from decimal import Decimal
+
+from store.models import Product
+
 
 class Basket():
 
@@ -10,13 +14,61 @@ class Basket():
         self.basket = basket
 
     def add(self, product, qty):
-        # Adding and updating the users basket session data
-        product_id = product.id
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = str(product.id)
 
-        if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
-        self.session.modified = True
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
+            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
+
+        self.save()
+
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the database
+        and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
 
     def __len__(self):
         # Get the basket data and count quantity of items
         return sum(item['qty'] for item in self.basket.values())
+
+    def update(self, product, qty):
+        """
+        Update values in session data
+        """
+        product_id = str(product)
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        self.save()
+
+    def get_total_price(self):
+            return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+    def delete(self, product):
+            """
+            Delete item from session data
+            """
+            product_id = str(product)
+
+            if product_id in self.basket:
+                del self.basket[product_id]
+                print(product_id)
+                self.save()
+
+    def save(self):
+        self.session.modified = True
